b4fce9ce8e2553f16367b7842c9071e69e28f8e8
diegoengelhard <00356119@uca.edu.sv>
2024-06-04 10:53:42 -0600

    add: react js app template

---
+++ my-react-app/src/views/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Temp from '../components/Temp'
+
+const Home = () => {
+    return (
+        <>
+            <div>Home</div>
+            <Temp />
+        </>
+    )
+}
+
+export default Home
